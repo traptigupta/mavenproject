e044c01b4efea44462b4ed82da72c45375147d93
traptigupta <trapti.gupta968@gmail.com>
2020-03-01 15:13:09 +0530

    Add files via upload

---
+++ src/main/java/CucumberWithSelenium/cucumber/selenium/cucumber.java
@@ -0,0 +1,8 @@
+package CucumberWithSelenium.cucumber.selenium;
+
+public class cucumber {
+	public static void main(String[] args) {
+		System.out
+				.println("Test.. Test by Crunchify.. \nThis is a simple tutorial on how to create .war file using Maven Plugin..");
+	}
+}
